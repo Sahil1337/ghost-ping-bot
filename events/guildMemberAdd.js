e36b97b9f30b2e386d9e49c13003e38e09e9df48
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-11-09 10:21:38 +0530

    💚 guildMemberAdd event

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,9 @@
+module.exports = async(client, member) => {
+    let channel = '769594179398402090';
+    let message = await
+    member.guild.channels.cache.get(channel).send(`${member}`)//its the message
+    setTimeout(async() => {
+        await message.delete();
+        console.log(`done ghost ping for ${member.user.username}`)
+    })
+}
